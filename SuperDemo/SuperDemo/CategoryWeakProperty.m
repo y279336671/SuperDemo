687b1a2b43b1637c2246c232b1fd10b92917a2c8
yanghe <yanghe@autohome.com.cn>
2020-11-17 14:59:02 +0800

    无用测试代码

--- SuperDemo/SuperDemo/CategoryWeakProperty.m
+++ SuperDemo/SuperDemo/CategoryWeakProperty.m
@@ -6,8 +6,18 @@
 //  Copyright © 2020 yanghe. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import "CategoryWeakProperty.h"
 //1. https://blog.csdn.net/yan_1564335/article/details/53996538    分类中的weak属性强引用一个对象A，weak属性被释放后会调用A的dealloc，delloc中回调给分类进行对属性的置nil处理
+@interface CategoryWeakProperty ()
+@property (nonatomic, weak)id weakObject;
+@end
 @implementation CategoryWeakProperty
-
+-(void)setWeakObject:(id)weakObject {
+//    objc_getAssociatedObject
+//    objc_setAssociatedObject(weakObject, <#const void * key#>, <#id value#>, OBJC_ASSOCIATION_RETAIN);
+}
+-(id)getWeakObject{
+//    return objc_getAssociatedObject(<#id object#>, <#const void * key#>)
+}
 @end
